314ff4e4f6c7fc9f8a731977a3a643319df70cf4
mrsid743 <siddhantpanekar39@gmail.com>
2025-09-28 19:19:36 +0530

    feat(terraform): Add RDS database resource definition

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,15 @@
-# outputs.tf
-
 output "alb_dns_name" {
-  description = "The DNS name of the Application Load Balancer."
-  value       = aws_lb.main.dns_name
+  description = "The DNS name of the ALB"
+  value       = aws_lb.strapi_alb.dns_name
+}
+
+output "ecs_cluster_name" {
+  description = "The name of the ECS cluster"
+  value       = aws_ecs_cluster.strapi_cluster.name
 }
 
 output "rds_endpoint" {
-  description = "The endpoint of the RDS database instance."
-  value       = aws_db_instance.main.endpoint
+  description = "The endpoint of the RDS instance"
+  value       = aws_db_instance.strapi_db.endpoint
 }
+
